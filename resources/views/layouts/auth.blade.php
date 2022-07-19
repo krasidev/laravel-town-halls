77b8b4ba4c4454b6ec7127f4dbc4a4ee3a5ce4bc
DEV-PC <dev@email.com>
2022-07-19 15:32:04 +0300

    Added 'PT Sans' font.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,10 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito" />
-
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
 
