3949be06fa2efc07f07edd58715bbc38ae538afe
DEV-PC <dev@email.com>
2022-07-23 13:21:19 +0300

    Added localizations and translations.

---
+++ resources/lang/bg/datatables.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'decimal' => '',
+    'emptyTable' => 'Няма данни',
+    'info' => 'Показване от _START_ до _END_ от _TOTAL_ записа',
+    'infoEmpty' => 'Показване от 0 до 0 от 0 записа',
+    'infoFiltered' => '(филтрирани от _MAX_ общо записа)',
+    'infoPostFix' => '',
+    'thousands' => ',',
+    'lengthMenu' => '_MENU_',
+    'loadingRecords' => 'Моля изчакайте - зареждане...',
+    'search' => '',
+    'searchPlaceholder' => 'Търси...',
+    'zeroRecords' => 'Няма намерени записи',
+    'aria' => [
+        'sortAscending' => '',
+        'sortDescending' => ''
+    ],
+    'selected_many' => 'избрани',
+    'selected' => 'избран'
+];
